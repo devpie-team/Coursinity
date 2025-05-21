67650cad4f95184da688e3651d06a4666c233640
MikhaylovskiyPavlo <mikhaylovskiy05@gmail.com>
2025-05-21 16:09:59 +0300

    added toggle and stepbar components

---
+++ src/components/Stepper/Stepper.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+
+type StepperProps = {
+  steps: number
+  activeStep: number
+  onStepClick?: (step: number) => void
+}
+
+const Stepper = ({ steps, activeStep, onStepClick }: StepperProps) => {
+  return (
+    <div className="grid grid-cols-3 gap-4 w-fit">
+      {Array.from({ length: steps }).map((_, i) => {
+        const step = i + 1
+        const isActive = step === activeStep
+
+        return (
+          <button
+            key={step}
+            onClick={() => onStepClick?.(step)}
+            className={`w-12 h-12 flex items-center justify-center rounded-full border-[1.5px] 
+              transition-colors text-white text-[24px] leading-8 font-medium
+              ${isActive ? 'border-white' : 'border-[#FFFFFF52]'}
+            `}>
+            {step}
+          </button>
+        )
+      })}
+    </div>
+  )
+}
+
+export default Stepper
---
+++ src/components/primitives/toggle.tsx
@@ -0,0 +1,40 @@
+import React from 'react'
+
+type ToggleProps = {
+  value: boolean // true = Ar, false = En
+  onToggle: (value: boolean) => void
+}
+
+const Toggle = ({ value, onToggle }: ToggleProps) => {
+  return (
+    <div className="relative w-[108px] h-[56px] rounded-full border border-gray-300 bg-white flex items-center justify-between  text-sm font-semibold">
+      {/* Текст (залишається завжди зверху) */}
+      <div className="flex justify-between px-[18px] w-full">
+        <span
+          className={`text-base leading-6 font-medium z-10 transition-colors ${value ? 'text-black' : 'text-white'}`}>
+          En
+        </span>
+        <span
+          className={`text-base leading-6 font-medium z-10 transition-colors ${value ? 'text-white' : 'text-black'}`}>
+          Ar
+        </span>
+      </div>
+
+      {/* Темний кружок */}
+      <div
+        className={`
+          absolute left-[3px] w-[48px] h-[48px] bg-black rounded-full transition-all duration-300
+          ${value ? 'translate-x-[52px]' : 'translate-x-0'}
+        `}
+      />
+
+      {/* Кнопки-клікери */}
+      <div className="absolute inset-0 z-20 flex">
+        <button className="flex-1" onClick={() => onToggle(false)} />
+        <button className="flex-1" onClick={() => onToggle(true)} />
+      </div>
+    </div>
+  )
+}
+
+export default Toggle
