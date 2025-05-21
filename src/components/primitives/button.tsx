63c4c5e0bbf8186049777db896d0480b36885385
MikhaylovskiyPavlo <mikhaylovskiy05@gmail.com>
2025-05-21 16:59:36 +0300

    added button components

--- src/components/primitives/button.tsx
+++ src/components/primitives/button.tsx
@@ -4,31 +4,24 @@ import { cva, type VariantProps } from 'class-variance-authority'
 
 import { cn } from '@/lib/utils'
 
-const buttonVariants = cva(
-  'inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0',
-  {
-    variants: {
-      variant: {
-        default: 'bg-primary text-primary-foreground hover:bg-primary/90',
-        destructive: 'bg-destructive text-destructive-foreground hover:bg-destructive/90',
-        outline: 'border border-input bg-background hover:bg-accent hover:text-accent-foreground',
-        secondary: 'bg-secondary text-secondary-foreground hover:bg-secondary/80',
-        ghost: 'hover:bg-accent hover:text-accent-foreground',
-        link: 'text-primary underline-offset-4 hover:underline'
-      },
-      size: {
-        default: 'h-10 px-4 py-2',
-        sm: 'h-9 rounded-md px-3',
-        lg: 'h-11 rounded-md px-8',
-        icon: 'h-10 w-10'
-      }
+const buttonVariants = cva('rounded-full leading-4 font-medium', {
+  variants: {
+    variant: {
+      primary:
+        'bg-black text-white hover:bg-[linear-gradient(90deg,_#0D0D0D_0%,_#3D3D3D_100%)] hover:shadow-[0px_12px_30px_0px_#A578F240] active:bg-none active:bg-primary-purple active:shadow-none disabled:pointer-events-none  disabled:bg-opacity-20 disabled:text-opacity-90 ',
+      secondary:
+        'bg-white backdrop-blur-[40px] text-black  hover:text-white hover:shadow-[0px_12px_40px_0px_#A578F240] hover:bg-primary-purple active:bg-transparent active:bg-[#8759D5] disabled:pointer-events-none  disabled:bg-opacity-30 disabled:text-opacity-40 disabled:text-white'
     },
-    defaultVariants: {
-      variant: 'default',
-      size: 'default'
+    size: {
+      md: 'h-14 px-6 py-5',
+      lg: 'h-16 px-8 py-5'
     }
+  },
+  defaultVariants: {
+    variant: 'primary',
+    size: 'md'
   }
-)
+})
 
 export interface ButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement>,
