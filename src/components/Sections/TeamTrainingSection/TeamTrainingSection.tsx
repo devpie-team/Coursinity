3d685c7e4be9c169739cde17672ee9a8e753e754
MikhaylovskiyPavlo <mikhaylovskiy05@gmail.com>
2025-05-24 15:05:06 +0300

    added adapt

--- src/components/Sections/TeamTrainingSection/TeamTrainingSection.tsx
+++ src/components/Sections/TeamTrainingSection/TeamTrainingSection.tsx
@@ -8,12 +8,16 @@ import { Typography } from '@/components/ui'
 export const TeamTrainingSection = () => {
   const t = useTranslations('TeamTrainingSection')
   const [isMobile, setIsMobile] = useState(false)
+  const [isTablet, setIsTablet] = useState(false)
+  const [isDesktop, setIsDesktop] = useState(true)
   const [selectedIndex, setSelectedIndex] = useState<number | null>(0)
 
   useEffect(() => {
     const checkScreenSize = () => {
       const width = window.innerWidth
       setIsMobile(width < 834)
+      setIsTablet(width < 1440)
+      setIsDesktop(width > 1440)
     }
 
     checkScreenSize()
@@ -30,34 +34,41 @@ export const TeamTrainingSection = () => {
   ]
 
   return (
-    <div className="flex pl-[140px] py-[85px] items-center ">
-      <div className="flex flex-col gap-10">
+    <div className="flex pl-[140px] py-[85px]  gap-[50px] max-[1440px]:px-6 max-[1440px]:pb-0 max-[1440px]:gap-5 max-[834px]:flex-col">
+      <div className="flex flex-col gap-10 ">
         <div className="flex flex-col gap-4">
-          <Typography variant="h3" weight="medium">
+          <Typography variant={isDesktop ? 'h3' : 'h5'} weight="medium">
             {t('title')}
           </Typography>
           <Typography variant="body3" weight="regular" className="text-description">
             {t('description')}
           </Typography>
         </div>
+        {selectedIndex !== null && (
+          <img
+            src={images[selectedIndex]}
+            alt={`Option ${selectedIndex + 1}`}
+            className="max-[834px]:block hidden w-full h-auto max-h-[300px] object-contain rounded-xl"
+          />
+        )}
 
-        <div className="grid grid-cols-2 grid-rows-2 gap-[20px]">
+        <div className="grid grid-cols-2 grid-rows-2 gap-[20px] max-[1440px]:flex max-[1440px]:flex-col ">
           {features.map((i) => {
             const isSelected = selectedIndex === i
             return (
               <div
                 key={i}
                 onClick={() => setSelectedIndex(i)}
-                className={`flex px-5 py-6 gap-4 border rounded-[20px] cursor-pointer transition-all text-black ${
+                className={`flex px-5 py-6 gap-4 border rounded-[20px] cursor-pointer transition-all text-black max-[1440px]:p-4 ${
                   isSelected ? 'bg-secondary-100 border-primary-purple ' : 'bg-white border-secondary-400 '
                 }`}>
                 <div
-                  className={`flex h-8 w-8 rounded-full  text-body1 font-medium leading-[150%] justify-center items-center shrink-0 ${
+                  className={`flex h-8 w-8 rounded-full  text-body1 font-medium leading-[150%] justify-center items-center shrink-0 max-[1440px]:h-6 max-[1440px]:w-6 max-[1440px]:text-caption ${
                     isSelected ? 'bg-primary-purple text-white' : ' bg-secondary-100 text-primary-purple'
                   }`}>
                   {i + 1}
                 </div>
-                <Typography variant="body3" weight="regular">
+                <Typography variant={isDesktop ? 'body3' : 'caption'} weight="regular" className="leading-[140%]">
                   {t(`items.${i}`)}
                 </Typography>
               </div>
@@ -65,17 +76,17 @@ export const TeamTrainingSection = () => {
           })}
         </div>
 
-        <Button variant="primary" className="w-[200px]">
+        <Button variant="primary" className="w-[200px] max-[1440px]:w-[343px]">
           {t('button')}
         </Button>
       </div>
 
-      <div className="w-full flex justify-center ">
+      <div className=" flex justify-center shrink-0 max-[834px]:hidden">
         {selectedIndex !== null && (
           <img
             src={images[selectedIndex]}
             alt={`Option ${selectedIndex + 1}`}
-            className="h-[670px] w-[680px] object-contain "
+            className="h-[670px] w-[680px] object-contain max-[1440px]:w-[400px] max-[1440px]:h-[410px] "
           />
         )}
       </div>
--- src/components/Sections/index.ts
+++ src/components/Sections/index.ts
@@ -1,3 +1,5 @@
+export { TeamTrainingSection } from '@/components/Sections/TeamTrainingSection';
+export { FutureSection } from './FutureSection/FutureSection';
 export { BuildSection } from './BuildSection/BuildSection'
 export { HeroSection } from './HeroSection'
 export { FeaturesSection } from './FeaturesSection'
