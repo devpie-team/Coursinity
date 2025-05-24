a1c90c5d45df6bc6ffda25b6fd61974a1d70ec26
MikhaylovskiyPavlo <mikhaylovskiy05@gmail.com>
2025-05-25 01:33:57 +0300

    future section with full anim, not full adpat

--- src/components/Sections/FutureSection/FutureSection.tsx
+++ src/components/Sections/FutureSection/FutureSection.tsx
@@ -18,12 +18,14 @@ export const FutureSection = () => {
 
   const [isMobile, setIsMobile] = useState(false)
   const [isTablet, setIsTablet] = useState(false)
+  const [isDesktop, setIsDesktop] = useState(false)
 
   useEffect(() => {
     const checkScreenSize = () => {
       const width = window.innerWidth
       setIsMobile(width < 768)
-      setIsTablet(width < 1024)
+      setIsTablet(width >= 768 && width <= 1024)
+      setIsDesktop(width > 1024)
     }
 
     checkScreenSize()
@@ -33,13 +35,13 @@ export const FutureSection = () => {
 
   useEffect(() => {
     const elementSettings = [
-      { selector: '.element1', x: 200, y: -700 },
-      { selector: '.element2', x: 1100, y: -200 },
-      { selector: '.element3', x: 1000, y: 700 },
-      { selector: '.element4', x: -1200, y: 600 },
-      { selector: '.element5', x: -1100, y: -300 }
+      { selector: '.future-element1', x: 200, y: -800 },
+      { selector: '.future-element2', x: 1100, y: -200 },
+      { selector: '.future-element3', x: 1000, y: 700 },
+      { selector: '.future-element4', x: -1000, y: 600 },
+      { selector: '.future-element5', x: -1100, y: -300 }
     ]
-
+    ScrollTrigger.getAll().forEach((trigger) => trigger.kill())
     // Для каждого элемента настраиваем анимацию
     elementSettings.forEach(({ selector, x, y }) => {
       gsap.fromTo(
@@ -50,14 +52,14 @@ export const FutureSection = () => {
           y: y
         },
         {
-          opacity: 1,
           x: 0,
           y: 0,
           ease: 'sine.inOut',
+          opacity: 1,
           scrollTrigger: {
             trigger: '.future-section',
             start: 'top top',
-            end: 'bottom top',
+            end: '130%',
             scrub: 2
           }
         }
@@ -66,38 +68,52 @@ export const FutureSection = () => {
     gsap.fromTo(
       '.scaleText',
       {
-        scale: 2
+        scale: isDesktop ? 3 : isTablet ? 2 : 1
       },
       {
-        scale: 0.5,
-        duration: 1,
+        scale: isMobile ? 0.7 : 1,
         ease: 'sine.inOut',
-        delay: 20,
         scrollTrigger: {
           trigger: '.future-section',
           start: 'bottom bottom',
-          end: 'bottom top ',
+          end: '90% ',
           scrub: 2,
           pin: true
         }
       }
     )
-  }, [])
+    gsap.fromTo(
+      '.opacityText',
+      { opacity: 1 },
+      {
+        opacity: 0,
+        ease: 'none',
+        scrollTrigger: {
+          trigger: '.future-section',
+          start: 'bottom 90%',
+          end: 'bottom 70%',
+          scrub: true
+        }
+      }
+    )
+  }, [isDesktop, isTablet, isMobile])
 
   return (
-    <div className="p-10  ">
-      <LogoSlider />
+    <div>
+      <div className="px-10">
+        <LogoSlider />
+      </div>
       <div className="relative flex items-center justify-center text-center w-full h-[975px] overflow-hidden future-section">
-        <div className="flex flex-col w-[452px] scaleText">
-          <Typography variant="h3" weight="medium">
+        <div className="flex flex-col w-[452px] scaleText opacityText">
+          <Typography variant={isDesktop ? 'h3' : 'h5'} weight="medium">
             The Future Starts Now. Get Your Team Ready
           </Typography>
-          <Typography variant="body2" weight="regular">
+          <Typography variant={isDesktop ? 'body2' : 'body3'} weight="regular">
             Get your team future-ready with training designed to ignite productivity.
           </Typography>
         </div>
 
-        <div className="absolute flex flex-col w-[240px] h-[240px] bg-primary-purple p-6 rounded-[40px] top-[40%] left-[40%]   element1">
+        <div className="absolute flex flex-col w-[240px] h-[240px]  bg-primary-purple p-6 rounded-[40px] max-lg:w-[160px] max-lg:h-[160px]    future-element1">
           <Typography variant="body2" weight="semibold" className="text-white ">
             Training Built to Fit
           </Typography>
@@ -116,7 +132,7 @@ export const FutureSection = () => {
           </div>
         </div>
 
-        <div className="absolute flex flex-col w-[250px] h-[250px] bg-secondary-100 p-6 rounded-[40px] justify-between text-start top-[40%] left-[40%]  element2">
+        <div className="opacity-1 absolute flex flex-col w-[250px] h-[250px] bg-secondary-100 p-6 rounded-[40px] justify-between text-start max-lg:w-[160px] max-lg:h-[160px]  future-element2 ">
           <Typography variant="body2" weight="semibold" className="text-primary-purple">
             Gamified Experiences that Stick
           </Typography>
@@ -125,7 +141,7 @@ export const FutureSection = () => {
           </div>
         </div>
 
-        <div className="absolute w-[250px] h-[250px] rounded-[40px] overflow-hidden top-[40%] left-[40%]  element3">
+        <div className="absolute w-[250px] h-[250px] rounded-[40px] overflow-hidden max-lg:w-[160px] max-lg:h-[160px]  future-element3">
           <div className="absolute inset-0 bg-[url('/assets/future/future_1.jpg')] bg-cover bg-center" />
           <div className="absolute inset-0 bg-gradient-to-b from-black/50 to-black/80" />
           <div className="relative z-1 p-6 pt-[33px] flex flex-col justify-between h-full text-white text-start">
@@ -136,7 +152,7 @@ export const FutureSection = () => {
           </div>
         </div>
 
-        <div className="absolute flex flex-col w-[240px] h-[240px] bg-secondary-200 p-6 rounded-[40px] justify-between top-[40%] left-[40%]  text-start element4">
+        <div className="absolute flex flex-col w-[240px] h-[240px] bg-secondary-200 p-6 rounded-[40px] justify-between    text-start max-lg:w-[160px] max-lg:h-[160px]  future-element4 ">
           <Typography variant="body2" weight="semibold" className="text-primary-blue">
             Seamless Systems by Design
           </Typography>
@@ -145,7 +161,7 @@ export const FutureSection = () => {
           </div>
         </div>
 
-        <div className="absolute flex flex-col w-[250px] h-[245px] bg-secondary-200 px-5 py-3 rounded-[40px] justify-end top-[40%] left-[40%]  text-start  bg-[url('/assets/future/future_3.jpg')] bg-cover bg-center shadow-[inset_0_0_0_1px_rgba(255,255,255,0.32)]  element5">
+        <div className="absolute flex flex-col w-[250px] h-[245px] bg-secondary-200 px-5 py-3 rounded-[40px] justify-end top-[40%]   text-start  bg-[url('/assets/future/future_3.jpg')] bg-cover bg-center shadow-[inset_0_0_0_1px_rgba(255,255,255,0.32)] max-lg:w-[160px] max-lg:h-[160px] future-element5">
           <div className="flex flex-3 bg-white rounded-[20px] p-3">
             <ZapIcon />
             <Typography variant="body2" weight="semibold">
