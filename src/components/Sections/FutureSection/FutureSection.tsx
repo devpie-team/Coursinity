979d843ac94c3261a0b6f68c030b9d8d73c19d49
MikhaylovskiyPavlo <mikhaylovskiy05@gmail.com>
2025-05-25 21:59:27 +0300

    fixes

--- src/components/Sections/FutureSection/FutureSection.tsx
+++ src/components/Sections/FutureSection/FutureSection.tsx
@@ -1,41 +1,45 @@
 'use client'
 
-import { useEffect, useState } from 'react'
+import { useEffect, useLayoutEffect } from 'react'
 import { useTranslations } from 'next-intl'
+import { gsap } from 'gsap'
+import { ScrollTrigger } from 'gsap/ScrollTrigger'
 
 import { LogoSlider } from './_components/LogoSlider'
 import { Typography } from '@/components/ui'
 import { UserOctagonIcon } from '@/components/icons/UserOctagonIcon'
-import { cn } from '@/lib/utils'
 import { ClipboardIcon } from '@/components/icons/ClipboardIcon'
 import { GridEditIcon } from '@/components/icons/GridEditIcon'
-import { gsap } from 'gsap'
-import { ScrollTrigger } from 'gsap/ScrollTrigger'
 import { ZapIcon } from '@/components/icons/ZapIcon'
+import { cn } from '@/lib/utils'
 
 export const FutureSection = () => {
   const t = useTranslations('FutureSection')
 
-  const [isMobile, setIsMobile] = useState(false)
-  const [isTablet, setIsTablet] = useState(false)
-  const [isDesktop, setIsDesktop] = useState(false)
-  const [windowWidth, setWindowWidth] = useState(0)
+  useLayoutEffect(() => {
+    gsap.registerPlugin(ScrollTrigger)
 
-  useEffect(() => {
-    const checkScreenSize = () => {
-      const width = window.innerWidth
-      setWindowWidth(width)
-      setIsMobile(width < 768)
-      setIsTablet(width >= 768 && width <= 1024)
-      setIsDesktop(width > 1024)
+    ScrollTrigger.matchMedia({
+      '(max-width: 767px)': () => {
+        runAnimations(0.7, 1.5)
+      },
+      '(min-width: 768px) and (max-width: 1024px)': () => {
+        runAnimations(1, 2)
+      },
+      '(min-width: 1025px) and (max-width: 1350px)': () => {
+        runAnimations(1, 2.3)
+      },
+      '(min-width: 1351px)': () => {
+        runAnimations(1, 3)
+      }
+    })
+
+    return () => {
+      ScrollTrigger.getAll().forEach((trigger) => trigger.kill())
     }
-
-    checkScreenSize()
-    window.addEventListener('resize', checkScreenSize)
-    return () => window.removeEventListener('resize', checkScreenSize)
   }, [])
 
-  useEffect(() => {
+  const runAnimations = (targetScale: number, initialScale: number) => {
     const elementSettings = [
       { selector: '.future-element1', x: 200, y: -800 },
       { selector: '.future-element2', x: 1100, y: -200 },
@@ -43,12 +47,6 @@ export const FutureSection = () => {
       { selector: '.future-element4', x: -1000, y: 600 },
       { selector: '.future-element5', x: -1100, y: -300 }
     ]
-    ScrollTrigger.getAll().forEach((trigger) => {
-      const el = trigger.trigger as Element
-      if (el?.classList?.contains('future-section')) {
-        trigger.kill()
-      }
-    })
 
     elementSettings.forEach(({ selector, x, y }) => {
       gsap.fromTo(
@@ -62,17 +60,13 @@ export const FutureSection = () => {
           scrollTrigger: {
             trigger: '.future-section',
             start: 'top top',
-            end: '130%',
+            end: '100%',
             scrub: 2
           }
         }
       )
     })
 
-    const initialScale =
-      windowWidth > 1350 ? 3 : windowWidth > 1024 ? 2.3 : windowWidth >= 768 ? 2 : windowWidth >= 400 ? 1.5 : 1
-
-    const targetScale = isMobile ? 0.7 : 1
     gsap.fromTo(
       '.scaleText',
       { scale: initialScale },
@@ -88,6 +82,7 @@ export const FutureSection = () => {
         }
       }
     )
+
     gsap.fromTo(
       '.opacityText',
       { opacity: 1 },
@@ -102,28 +97,24 @@ export const FutureSection = () => {
         }
       }
     )
-  }, [isDesktop, isTablet, isMobile, windowWidth])
+  }
 
   return (
     <section>
-      <div className="px-10">
-        <LogoSlider />
-      </div>
+      <LogoSlider />
+
       <div className="relative flex items-center justify-center text-center w-full h-[975px] overflow-hidden future-section">
         <div className="flex flex-col max-w-full w-[432px] scaleText opacityText max-lg:w-[372px] max-md:w-[252px]">
-          <Typography variant={isDesktop ? 'h3' : 'h5'} weight="medium">
+          <Typography variant="h3" weight="medium">
             {t('title')}
           </Typography>
-          <Typography variant={isDesktop ? 'body2' : 'body3'} weight="regular" className="text-description">
+          <Typography variant="body2" weight="regular" className="text-description">
             {t('subtitle')}
           </Typography>
         </div>
 
         <div className="absolute flex flex-col w-[240px] h-[240px] bg-primary-purple p-6 rounded-[40px] max-lg:w-[160px] max-lg:h-[160px] max-lg:p-3 max-lg:pb-2 max-lg:rounded-2xl future-element1">
-          <Typography
-            variant={isDesktop ? 'body2' : 'caption'}
-            weight={isDesktop ? 'semibold' : 'medium'}
-            className="text-white">
+          <Typography variant="body2" weight="semibold" className="text-white">
             {t('card1Title')}
           </Typography>
           <div className="flex gap-1 h-full items-end max-lg:gap-[3px]">
@@ -132,35 +123,29 @@ export const FutureSection = () => {
                 key={i}
                 className={cn(
                   'flex justify-center items-end bg-white bg-opacity-[16%]',
-                  isDesktop ? 'h-[101px] p-2 rounded-[15px] w-[45px]' : 'h-[92px] p-1 w-[32px] rounded-[10px]',
+                  'h-[101px] p-2 rounded-[15px] w-[45px]',
                   i === 1 && 'items-start',
                   i === 2 && 'items-center'
                 )}>
-                <UserOctagonIcon size={isDesktop ? '32' : '22.67'} />
+                <UserOctagonIcon size="32" />
               </div>
             ))}
           </div>
         </div>
 
         <div className="opacity-1 absolute flex flex-col w-[250px] h-[250px] bg-secondary-100 p-6 rounded-[40px] justify-between text-start max-lg:w-[160px] max-lg:h-[160px] max-lg:p-3 max-lg:pt-2 max-lg:rounded-2xl future-element2">
-          <Typography
-            variant={isDesktop ? 'body2' : 'caption'}
-            weight={isDesktop ? 'semibold' : 'medium'}
-            className="text-primary-purple">
+          <Typography variant="body2" weight="semibold" className="text-primary-purple">
             {t('card2Title')}
           </Typography>
           <div className="flex w-20 h-20 bg-primary-purple justify-center items-center rounded-full max-lg:w-12 max-lg:h-12">
-            <ClipboardIcon size={isDesktop ? '48' : '29'} />
+            <ClipboardIcon size="48" />
           </div>
         </div>
 
         <div className="absolute w-[250px] h-[250px] rounded-[40px] overflow-hidden p-6 bg-[url('/assets/future/future_1.jpg')] bg-cover bg-center max-lg:w-[160px] max-lg:h-[160px] max-lg:p-3 max-lg:pt-2 max-lg:rounded-2xl future-element3">
           <div className="absolute inset-0 bg-gradient-to-b from-black/50 to-black/80" />
           <div className="relative flex flex-col justify-between h-full text-white text-start">
-            <Typography
-              variant={isDesktop ? 'body2' : 'caption'}
-              weight={isDesktop ? 'semibold' : 'medium'}
-              className="text-white">
+            <Typography variant="body2" weight="semibold" className="text-white">
               {t('card3Title')}
             </Typography>
             <img src="/assets/future/future_2.png" alt="" className="object-fill" />
@@ -168,23 +153,20 @@ export const FutureSection = () => {
         </div>
 
         <div className="absolute flex flex-col w-[240px] h-[240px] bg-secondary-200 p-6 rounded-[40px] justify-between text-start max-lg:w-[160px] max-lg:h-[160px] max-lg:p-3 max-lg:pt-2 max-lg:rounded-2xl future-element4">
-          <Typography
-            variant={isDesktop ? 'body2' : 'caption'}
-            weight={isDesktop ? 'semibold' : 'medium'}
-            className="text-primary-blue">
+          <Typography variant="body2" weight="semibold" className="text-primary-blue">
             {t('card4Title')}
           </Typography>
           <div className="flex w-20 h-20 bg-primary-blue justify-center items-center rounded-full max-lg:w-12 max-lg:h-12">
-            <GridEditIcon size={isDesktop ? '48' : '29'} />
+            <GridEditIcon size="48" />
           </div>
         </div>
 
         <div className="absolute flex flex-col w-[250px] h-[245px] bg-secondary-200 px-5 py-3 rounded-[40px] justify-end text-start bg-[url('/assets/future/future_3.jpg')] bg-cover bg-center shadow-[inset_0_0_0_1px_rgba(255,255,255,0.32)] max-lg:w-[160px] max-lg:h-[160px] max-lg:p-3 max-lg:pb-0 max-lg:rounded-2xl items-center future-element5">
           <div className="flex bg-white rounded-[20px] p-3 max-lg:p-2 max-lg:rounded-xl max-lg:w-[144px]">
             <div>
-              <ZapIcon size={isDesktop ? '24' : '14'} />
+              <ZapIcon size="24" />
             </div>
-            <Typography variant={isDesktop ? 'body2' : 'caption'} weight={isDesktop ? 'semibold' : 'medium'}>
+            <Typography variant="body2" weight="semibold">
               {t('card5Title')}
             </Typography>
           </div>
