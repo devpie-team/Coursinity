cb597454c5d11341f498727f144e3f3b6082aaed
MikhaylovskiyPavlo <mikhaylovskiy05@gmail.com>
2025-05-26 19:53:16 +0300

    fixed links and apdative on footer

--- src/components/ui/Typography/Typography.tsx
+++ src/components/ui/Typography/Typography.tsx
@@ -1,14 +1,19 @@
 import clsx from 'clsx'
+import { ElementType, ReactNode, ComponentPropsWithoutRef } from 'react'
 
-type TypographyProps = {
-  as?: 'p' | 'span' | 'h1' | 'h2' | 'h3' | 'h4'
-  variant?: 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6' | 'body1' | 'body2' | 'body3' | 'body4' | 'button' | 'caption'
-  weight?: 'regular' | 'medium' | 'semibold' | 'bold'
-  children: React.ReactNode
+type Variant = 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6' | 'body1' | 'body2' | 'body3' | 'body4' | 'button' | 'caption'
+
+type Weight = 'regular' | 'medium' | 'semibold' | 'bold'
+
+type TypographyProps<T extends ElementType = 'p'> = {
+  as?: T
+  variant?: Variant
+  weight?: Weight
   className?: string
-}
+  children: ReactNode
+} & ComponentPropsWithoutRef<T>
 
-const variantMap = {
+const variantMap: Record<Variant, string> = {
   h1: 'text-h1',
   h2: 'text-h2',
   h3: 'text-h3',
@@ -21,24 +26,26 @@ const variantMap = {
   body4: 'text-body4',
   button: 'button',
   caption: 'text-caption'
-} as const
+}
 
-const weightMap = {
+const weightMap: Record<Weight, string> = {
   regular: 'font-normal',
   medium: 'font-medium',
   semibold: 'font-semibold',
   bold: 'font-bold'
 }
 
-export const Typography = ({
-  as: Component = 'p',
+export const Typography = <T extends ElementType = 'p'>({
+  as,
   variant = 'body1',
   weight = 'regular',
+  className,
   children,
-  className = ''
-}: TypographyProps) => {
+  ...props
+}: TypographyProps<T>) => {
+  const Component = as || 'p'
   return (
-    <Component className={clsx(variantMap[variant], weightMap[weight], 'font-poppins', className)}>
+    <Component className={clsx(variantMap[variant], weightMap[weight], 'font-poppins', className)} {...props}>
       {children}
     </Component>
   )
