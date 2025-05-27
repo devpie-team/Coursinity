66bcc0c098467626c17c079a85f44fb5851dbe10
Lokiner890 <rostick@offerwell.com>
2025-05-27 22:13:19 +0300

    Finished main part of impact sections

--- src/components/Sections/ImpactSection/ImpactSection.tsx
+++ src/components/Sections/ImpactSection/ImpactSection.tsx
@@ -22,7 +22,7 @@ export const ImpactSection = () => {
       { x: 0 },
       {
         x: -wrapperWidth,
-        duration: 10,
+        duration: 12,
         ease: 'linear',
         repeat: -1
       }
@@ -41,15 +41,24 @@ export const ImpactSection = () => {
       <div ref={containerRef} className="relative h-[672px] min-w-[1705px] bg-black overflow-hidden">
         <div ref={wrapperRef} className="absolute flex">
           {[...Array(2)].map((_, groupIdx) => (
-            <div key={groupIdx} className="relative flex-shrink-0" style={{ width: '1705px', height: '633px' }}>
-              <div className="blue-gradient-border absolute w-[362px] h-[280px] top-[146px] left-0"></div>
+            <div
+              key={groupIdx}
+              className="relative flex-shrink-0 text-white"
+              style={{ width: '1705px', height: '633px' }}>
+              <div className="blue-gradient-border absolute w-[362px] h-[280px] top-[146px] left-0  flex flex-col gap-[28px] p-6 items-end">
+                <div className="flex flex-col gap-2">
+                  <Typography variant="h3" weight="medium">
+                    12+
+                  </Typography>
+                  <Typography variant="body3">{t('sectors')}</Typography>
+                </div>
+                <img src="/assets/impact/folder.png" alt="Folder" className="w-[157px] h-[128px]" />
+              </div>
               <div className="blue-gradient-border absolute w-[253px] h-[65px] top-[22px] left-[162px] p-[6px] flex items-center before:rounded-[54px]  gap-[9px]">
                 <div className="flex items-center justify-center w-[53px] h-[53px] rounded-full bg-primary-blue">
-                  <Typography variant="h5" className="text-white">
-                    #
-                  </Typography>
+                  <Typography variant="h5">#</Typography>
                 </div>
-                <Typography variant="body1" weight="medium" className="text-white">
+                <Typography variant="body1" weight="medium">
                   {t('smart')}
                 </Typography>
               </div>
@@ -59,7 +68,7 @@ export const ImpactSection = () => {
               <div className="blue-gradient-border absolute w-[100px] h-[100px] top-[30px] right-[50px] flex items-center justify-center">
                 <BuildingIcon />
               </div>
-              <div className="blue-gradient-border absolute w-[269px] h-[160px] top-[93px] left-[488px] p-6 flex flex-col gap-2 text-white">
+              <div className="blue-gradient-border absolute w-[269px] h-[160px] top-[93px] left-[488px] p-6 flex flex-col gap-2 ">
                 <Typography variant="h3" weight="medium">
                   725
                 </Typography>
@@ -68,8 +77,25 @@ export const ImpactSection = () => {
               <div className="blue-gradient-border absolute w-[156px] h-[52px] bottom-[151px] left-[421px] flex items-center justify-center before:rounded-[54px]">
                 <StarsIcon />
               </div>
-              <div className="blue-gradient-border absolute w-[362px] h-[316px] bottom-0 left-[619px]"></div>
-              <div className="blue-gradient-border absolute w-[309px] h-[223px] top-[35px] right-[556px]"></div>
+              <div className="overflow-hidden blue-gradient-border absolute w-[362px] h-[316px] bottom-0 left-[619px] flex flex-col gap-[28px] p-6 items-end ">
+                <div className="flex flex-col gap-2">
+                  <Typography variant="h3" weight="medium">
+                    6
+                  </Typography>
+                  <Typography variant="body3">{t('active')}</Typography>
+                </div>
+                <img
+                  src="/assets/impact/planet.png"
+                  alt="Planet"
+                  className="w-[228px] h-[228px] absolute bottom-[-17px] right-[-21px]"
+                />
+              </div>
+              <div className="blue-gradient-border absolute w-[309px] h-[223px] top-[35px] right-[556px] flex flex-col gap-[23px] px-8 py-[38px]">
+                <Typography variant="h3" weight="medium">
+                  230K+
+                </Typography>
+                <Typography variant="body3">{t('learners')}</Typography>
+              </div>
               <div className="blue-gradient-border absolute w-[100px] h-[100px] top-0 right-[438px] flex items-center justify-center ">
                 <EducationIcon />
               </div>
@@ -78,18 +104,22 @@ export const ImpactSection = () => {
               </div>
               <div className="blue-gradient-border absolute w-[296px] h-[65px] bottom-[231px] right-[390px] flex items-center before:rounded-[54px]  gap-[9px]">
                 <div className="flex items-center justify-center w-[53px] h-[53px] rounded-full bg-primary-blue">
-                  <Typography variant="h5" className="text-white">
-                    #
-                  </Typography>
+                  <Typography variant="h5">#</Typography>
                 </div>
-                <Typography variant="body1" weight="medium" className="text-white">
+                <Typography variant="body1" weight="medium">
                   {t('numbers')}
                 </Typography>
               </div>
               <div className="blue-gradient-border absolute w-[100px] h-[100px] bottom-[58px] right-[507px] flex items-center justify-center">
                 <SmileIcon />
               </div>
-              <div className="blue-gradient-border absolute w-[362px] h-[308px] bottom-[27px] right-[16px]"></div>
+              <div className="blue-gradient-border absolute w-[362px] h-[308px] bottom-[27px] right-[16px] flex flex-col gap-3 px-[26px] py-[31px]">
+                <Typography variant="h3" weight="medium">
+                  9.6
+                </Typography>
+                <Typography variant="body3">{t('average')}</Typography>
+                <img src="/assets/impact/image.png" alt="Graph" className="w-full" />
+              </div>
             </div>
           ))}
         </div>
