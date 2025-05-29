ccd8a8681d570b330762d58348b99127d7cbd42a
MikhaylovskiyPavlo <mikhaylovskiy05@gmail.com>
2025-05-29 19:54:49 +0300

    fix

---
+++ src/components/Sections/InspirationSection/_components/ExpandableCard/ExpandableCard.tsx
@@ -0,0 +1,35 @@
+import { cn } from '@/lib/utils'
+import { space } from 'postcss/lib/list'
+
+type ExpandableCardProps = {
+  title: string
+  description?: string
+  icon?: React.ReactNode
+  isOpen?: boolean
+  onClick?: () => void
+}
+
+export const ExpandableCard = ({ title, description, icon, isOpen = false, onClick }: ExpandableCardProps) => {
+  return (
+    <div
+      className={cn(
+        'flex flex-col bg-[linear-gradient(180deg,_rgba(255,255,255,0.06)_0%,_rgba(30,141,194,0.06)_100%)] text-white rounded-[20px] px-6 py-4 ',
+        isOpen && ''
+      )}
+      onClick={onClick}>
+      <div className="flex flex-col  gap-4">
+        <div className={cn('flex flex-row gap-6', isOpen && 'gap-8 flex-col')}>
+          {isOpen && icon && (
+            <span className="flex bg-primary-blue bg-opacity-5 h-20 w-20 justify-center items-center rounded-full">
+              {icon}
+            </span>
+          )}
+          {!isOpen && <div className="w-1 h-6 bg-white opacity-40 rounded-[40px]"></div>}
+          <span>{title}</span>
+        </div>
+
+        {isOpen && <div className="  opacity-65 ">{description}</div>}
+      </div>
+    </div>
+  )
+}
