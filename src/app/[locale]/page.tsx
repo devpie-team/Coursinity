90d982173dbcfdf80dd6ffd5abecc4c285c75bad
Lokiner890 <rostick@offerwell.com>
2025-06-04 15:52:01 +0300

    Init header

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -29,7 +29,6 @@ export default function HomePage() {
       <GrowthStepSection />
       <AiTeamSection />
       <InspirationSection />
-
       <BuildSection />
       <ImpactSection />
       <TeamTrainingSection />
