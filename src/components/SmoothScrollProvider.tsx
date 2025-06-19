25dd8c50cd927ab41bed5cf8154948fde1b385bd
MikhaylovskiyPavlo <mikhaylovskiy05@gmail.com>
2025-06-19 17:30:12 +0300

    added smoth scroll

---
+++ src/components/SmoothScrollProvider.tsx
@@ -0,0 +1,33 @@
+'use client'
+import { useEffect } from 'react'
+import Lenis from '@studio-freight/lenis'
+
+declare global {
+  interface Window {
+    lenis?: Lenis
+  }
+}
+
+export default function SmoothScrollProvider({ children }: { children: React.ReactNode }) {
+  useEffect(() => {
+    const lenis = new Lenis({
+      lerp: 0.09,
+      smoothWheel: true,
+      touchMultiplier: 1.5,
+      infinite: false
+    })
+    window.lenis = lenis
+
+    function raf(time: number) {
+      lenis.raf(time)
+      requestAnimationFrame(raf)
+    }
+    requestAnimationFrame(raf)
+    return () => {
+      lenis.destroy()
+      delete window.lenis
+    }
+  }, [])
+
+  return <>{children}</>
+}
