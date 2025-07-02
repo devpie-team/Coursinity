d6007f0c1723107c7ae7bd37630ba4a3c96d2870
MikhaylovskiyPavlo <mikhaylovskiy05@gmail.com>
2025-07-02 14:48:18 +0300

    fix

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -10,7 +10,7 @@ export async function generateMetadata({ params }: { params: Promise<{ locale: s
       ? 'أفضل شركات التدريب في السعودية | Coursinity'
       : 'Best Training Companies in Saudi Arabia | Coursinity',
     description: isArabic
-      ? 'استكشف COURSINITY: دورات تدريبية مُجزأة تجمع بين الخبرة الحياتية والتعلم التفاعلي، للحصول على أفضل الشهادات الاحترافية والتدريب المعتمد دوليا في السعودية.'
+      ? 'الرائدة في رفع إنتاجية أعضاء فريقك بأحدث البرامج والتقنيات'
       : 'Explore COURSINITY: Blended courses that combine real-life experience and interactive learning, for the best professional certifications and internationally accredited training in Saudi Arabia.',
     icons: {
       icon: '/assets/favicon.png',
