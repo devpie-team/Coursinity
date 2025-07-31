5aa918d999f71b9df476f49266adb6dbc300540c
Lokiner890 <rostick@offerwell.com>
2025-07-31 20:05:42 +0300

    Video section and adaptive fixes

--- src/app/[locale]/solutions/page.tsx
+++ src/app/[locale]/solutions/page.tsx
@@ -57,7 +57,7 @@ export default function HomePage() {
 
   return (
     <>
-      {/*  {loading && <Loader loading={loading} />} */}
+      {/* {loading && <Loader loading={loading} />} */}
 
       <Header />
       <div className="relative">
@@ -71,7 +71,7 @@ export default function HomePage() {
       <StepScroll />
 
       {/*   <CardSection /> */}
-      <CultureAlignedSection />
+      {/* <CultureAlignedSection /> */}
       <CustomTrainingSection />
       <PathSection />
       <ConsultingSection />
--- src/components/SolutionsSections/HeroSection/_components/VideoSection.tsx
+++ src/components/SolutionsSections/HeroSection/_components/VideoSection.tsx
@@ -1,11 +1,26 @@
 'use client'
+import React from 'react'
+import ReactPlayer from 'react-player'
 import './VideoSection.style.css'
+
 export const VideoSection = () => {
   return (
-    <section className="relative flex items-center justify-center ] mt-[60px] h-[650px] overflow-hidden">
-      <div className=" absolute bg-white/20 rounded-t-[20px] w-[850px] h-[600px] backdrop-blur-[72px] z-30  bottom-0 custom-shadow"></div>
-      <div className="absolute bg-gradient-to-b from-white/10 to-white/0 rounded-t-[20px] w-[880px] h-[505px] backdrop-blur-[40px]  z-20  bottom-[40px]"></div>
-      <div className="absolute bg-gradient-to-b from-white/5 to-white/0 rounded-t-[20px] w-[910px] h-[540px]   bottom-[-40px] z-10 shadow-[0_4px_80px_16px_rgba(9,19,21,0.16)]"></div>
+    <section className="relative flex items-center justify-center mt-[60px] max-md:mb-[-10px] h-[650px] max-lg:h-[515px] max-md:h-[290px] overflow-hidden z-0">
+      <div className="absolute flex flex-col items-center justify-between px-2 bg-white/20 rounded-t-[20px] w-[850px] max-lg:w-[675px] max-md:w-[350px] h-[600px] max-lg:h-[480px] max-md:h-[250px] backdrop-blur-[72px] z-30 bottom-0 custom-shadow ">
+        <img src="/Toolbar.svg" className="w-full" />
+        <ReactPlayer
+          src="/assets/video/heroVideo.mp4"
+          width="100%"
+          height="auto"
+          playing
+          muted={true}
+          loop
+          controls={false}
+          style={{ borderRadius: '16px 16px 0 0', overflow: 'hidden' }}
+        />
+      </div>
+      <div className="absolute bg-gradient-to-b from-white/10 to-white/0 rounded-t-[20px] w-[880px] max-lg:w-[700px] max-md:w-[360px] h-[505px] max-lg:h-[402px] max-md:h-[208px] backdrop-blur-[40px]  z-20  bottom-[40px]"></div>
+      <div className="absolute bg-gradient-to-b from-white/5 to-white/0 rounded-t-[20px] w-[910px] max-lg:w-[725px] max-md:w-[375px] h-[540px] max-lg:h-[433px] max-md:h-[224px] bottom-[-40px] z-10 shadow-[0_4px_80px_16px_rgba(9,19,21,0.16)]"></div>
     </section>
   )
 }
