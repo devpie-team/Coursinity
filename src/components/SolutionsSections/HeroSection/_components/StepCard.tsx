97ab4ee1714865890d7f5adc60c4b3c15c6944bd
MikhaylovskiyPavlo <mikhaylovskiy05@gmail.com>
2025-08-04 14:38:06 +0300

    fixes in heroSection

--- src/components/SolutionsSections/HeroSection/_components/StepCard.tsx
+++ src/components/SolutionsSections/HeroSection/_components/StepCard.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import { GradientTickIcon } from '@/components/icons/GradientTickIcon'
 import { Typography } from '@/components/ui'
 import React, { forwardRef, useEffect, useRef, useState } from 'react'
@@ -10,15 +12,16 @@ type StepCardProps = {
   isLast?: boolean
   isCompleted?: boolean
   onClick?: () => void
+  openPercent?: number
 }
 
 export const StepCard = forwardRef<HTMLDivElement, StepCardProps>(
-  ({ title, description, number, isOpen, isLast = false, isCompleted = false, onClick }, ref) => {
+  ({ title, description, number, isOpen, isLast = false, isCompleted = false, onClick, openPercent = 0 }, ref) => {
     const localRef = useRef<HTMLDivElement | null>(null)
-    const [openPercent, setOpenPercent] = useState(0)
     const [isMobile, setIsMobile] = useState(false)
     const [isTablet, setIsTablet] = useState(false)
     const [isDesktop, setIsDesktop] = useState(true)
+
     useEffect(() => {
       const checkScreenSize = () => {
         const width = window.innerWidth
@@ -30,29 +33,12 @@ export const StepCard = forwardRef<HTMLDivElement, StepCardProps>(
       window.addEventListener('resize', checkScreenSize)
       return () => window.removeEventListener('resize', checkScreenSize)
     }, [])
-    const CLOSED_HEIGHT = 86
-    const OPEN_HEIGHT = isDesktop ? 150 : 120
 
     const setRefs = (el: HTMLDivElement | null) => {
       if (typeof ref === 'function') ref(el)
       else if (ref) (ref as React.MutableRefObject<HTMLDivElement | null>).current = el
       localRef.current = el
     }
-
-    useEffect(() => {
-      const update = () => {
-        const node = localRef.current
-        if (!node) return
-        const height = node.offsetHeight
-        const percent = Math.max(0, Math.min(1, (height - CLOSED_HEIGHT) / (OPEN_HEIGHT - CLOSED_HEIGHT)))
-        setOpenPercent(percent)
-      }
-
-      update()
-      const ro = new ResizeObserver(update)
-      if (localRef.current) ro.observe(localRef.current)
-      return () => ro.disconnect()
-    }, [])
 
     return (
       <div
@@ -63,7 +49,7 @@ export const StepCard = forwardRef<HTMLDivElement, StepCardProps>(
         }}>
         <div className="flex gap-4 items-start cursor-pointer" onClick={onClick}>
           <div className="flex flex-col w-6 min-w-[24px] items-center relative">
-            <Typography variant={isDesktop ? 'h4' : 'h6'} weight="medium" className=" flex items-center justify-center">
+            <Typography variant={isDesktop ? 'h4' : 'h6'} weight="medium" className="flex items-center justify-center">
               <span
                 className={`flex justify-center items-center transition-all duration-300 ease-out ${
                   isCompleted ? 'scale-100 opacity-100' : 'scale-0 opacity-0'
@@ -80,10 +66,10 @@ export const StepCard = forwardRef<HTMLDivElement, StepCardProps>(
 
             {!isLast && (
               <span
-                className="absolute left-1/2 top-full translate-x-[-50%] rounded-full transition-[height,margin-top,background] duration-1000"
+                className="absolute left-1/2 top-full translate-x-[-50%] rounded-full"
                 style={{
-                  marginTop: isOpen ? '6px' : '12px',
-                  height: isOpen ? ` ${isDesktop ? 96 : 64}px` : '20px',
+                  marginTop: `${12 - openPercent * 6}px`,
+                  height: `${20 + openPercent * (isDesktop ? 76 : 44)}px`,
                   width: '3px',
                   background: isOpen
                     ? 'linear-gradient(180deg, rgba(30,141,194,0.64) -7.93%, rgba(165,120,242,0.64) 25.83%, rgba(30,141,194,0.64) 105.37%)'
@@ -99,8 +85,8 @@ export const StepCard = forwardRef<HTMLDivElement, StepCardProps>(
             </Typography>
             <Typography
               variant="body2"
-              className="text-description transition-opacity duration-0"
-              style={{ opacity: openPercent }}>
+              className="text-description transition-opacity duration-1000"
+              style={{ opacity: isOpen ? 1 : 0 }}>
               {description}
             </Typography>
           </div>
