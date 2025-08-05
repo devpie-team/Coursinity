d3f63e07854dc4796e33545640128c4124a21e5c
MikhaylovskiyPavlo <mikhaylovskiy05@gmail.com>
2025-08-05 18:46:28 +0300

    fixed bg transition

--- src/components/SolutionsSections/HeroSection/_components/VideoSection.tsx
+++ src/components/SolutionsSections/HeroSection/_components/VideoSection.tsx
@@ -28,8 +28,8 @@ export const VideoSection = () => {
   return (
     <section
       ref={sectionRef}
-      className="relative flex items-center justify-center mt-[60px] max-md:mb-[-10px] h-[650px] max-lg:h-[515px] max-md:h-[290px] overflow-hidden z-0 bottom">
-      <div className="absolute flex flex-col items-center justify-between px-2 bg-white/20 rounded-t-[20px] max-md:rounded-t-lg  w-[850px] max-lg:w-[675px] max-md:w-[350px] h-[600px] max-lg:h-[480px] max-md:h-[250px] backdrop-blur-[72px] z-30 bottom-0 custom-shadow ">
+      className="relative flex items-center justify-center mt-[60px] max-md:mb-[-10px] h-[650px] max-lg:h-[515px] max-md:h-[290px]  z-0 ">
+      <div className="absolute flex flex-col items-center justify-between px-2 bg-white/20 rounded-t-[20px] max-md:rounded-t-lg  w-[850px] max-lg:w-[675px] max-md:w-[350px] h-[600px] max-lg:h-[480px] max-md:h-[250px] backdrop-blur-[72px] z-30 bottom-[-2] max-lg:bottom-[-5]  custom-shadow ">
         <Lottie
           lottieRef={lottieRef}
           animationData={hero}
