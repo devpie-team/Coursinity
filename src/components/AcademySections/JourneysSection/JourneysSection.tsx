e590ed5ff9c5a4d43316fc99ad1250b4d8395ce0
MikhaylovskiyPavlo <mikhaylovskiy05@gmail.com>
2025-08-28 19:35:27 +0300

    added arabic version

--- src/components/AcademySections/JourneysSection/JourneysSection.tsx
+++ src/components/AcademySections/JourneysSection/JourneysSection.tsx
@@ -2,7 +2,6 @@
 
 import { ListIcon } from '@/components/icons'
 import { ChartNotificationIcon } from '@/components/icons/ChartNotificationIcon'
-
 import { PyramideIcon } from '@/components/icons/PyramideIcon'
 import { RankIcon } from '@/components/icons/RankIcon'
 import TeacherIcon from '@/components/icons/TeacherIcon'
@@ -15,7 +14,7 @@ import { useInView } from 'react-intersection-observer'
 
 import lottie1En from '../../../../public/assets/lottie/academy/journey_section/journey_1.json'
 import lottie2En from '../../../../public/assets/lottie/academy/journey_section/journey_2.json'
-import { useLocale } from 'next-intl'
+import { useLocale, useTranslations } from 'next-intl'
 
 export const JourneySection = () => {
   const [isMobile, setIsMobile] = useState(false)
@@ -23,11 +22,14 @@ export const JourneySection = () => {
   const [isDesktop, setIsDesktop] = useState(true)
   const locale = useLocale()
   const isArabic = locale === 'ar'
+  const t = useTranslations('AC_JourneySection')
+
   const lottieRef1 = useRef<LottieRefCurrentProps>(null)
   const lottieRef2 = useRef<LottieRefCurrentProps>(null)
 
   const { ref: ref1, inView: inView1 } = useInView({ triggerOnce: false })
   const { ref: ref2, inView: inView2 } = useInView({ triggerOnce: false })
+
   useEffect(() => {
     const checkScreenSize = () => {
       const width = window.innerWidth
@@ -53,67 +55,69 @@ export const JourneySection = () => {
       lottieRef2.current.play()
     }
   }, [inView2])
+
   return (
-    <section className="flex flex-col gap-10 py-[120px] bg-white  justify-center items-center px-4 max-lg:py-[80px]">
-      <div className="flex flex-col gap-6 text-center">
+    <section className="flex flex-col gap-10 py-[120px] bg-white justify-center items-center px-4 max-lg:py-[80px]">
+      <div className="flex flex-col gap-6 text-center max-w-[750px]">
         <Typography variant={isDesktop ? 'h3' : 'h5'} weight="medium">
-          Your Training Academy, Built by You
+          {t('title')}
         </Typography>
         <Typography variant="body3" weight="regular" className="text-description">
-          Coursinity Academy is your team’s all-in-one hub for skills, knowledge, and real-world learning.
+          {t('subtitle')}
         </Typography>
       </div>
 
       <div className="flex gap-6 max-md:flex-col">
-        <div className="bg-secondary-300 rounded-2xl max-w-[565px]  p-8 max-md:max-w-full">
+        {/* Left card */}
+        <div className="bg-secondary-300 rounded-2xl max-w-[565px] p-8 max-md:max-w-full">
           <div className="flex flex-col gap-6">
             <Typography variant={isDesktop ? 'h5' : 'h6'} weight="medium">
-              Interactive Training Journeys
+              {t('cardsLeft.title')}
             </Typography>
             <div className="flex flex-col gap-4">
               <div className="flex gap-4">
                 <div className="flex w-10 h-10 items-center justify-center bg-secondary-200 rounded-lg border-primary-blue/20 border shrink-0">
                   <RankIcon />
                 </div>
-
                 <div className="flex flex-col gap-2">
                   <Typography variant={isDesktop ? 'body2' : 'body3'} weight="medium">
-                    Ongoing Adventures
+                    {t('cardsLeft.items.0.title')}
                   </Typography>
                   <Typography variant="body3" className="text-description">
-                    Accelerate skills with engaging challenges, gamification, and instant expert support.
+                    {t('cardsLeft.items.0.description')}
                   </Typography>
                 </div>
               </div>
+
               <div className="flex gap-4">
                 <div className="flex w-10 h-10 items-center justify-center bg-secondary-200 rounded-lg border-primary-blue/20 border shrink-0">
                   <PyramideIcon />
                 </div>
-
                 <div className="flex flex-col gap-2">
                   <Typography variant="body2" weight="medium">
-                    Complete Roadmaps
+                    {t('cardsLeft.items.1.title')}
                   </Typography>
                   <Typography variant="body3" className="text-description">
-                    Step-by-step plans that keep your team charging forward.
+                    {t('cardsLeft.items.1.description')}
                   </Typography>
                 </div>
               </div>
+
               <div className="flex gap-4">
                 <div className="flex w-10 h-10 items-center justify-center bg-secondary-200 rounded-lg border-primary-blue/20 border shrink-0">
                   <UsersIcon color="#1C8DC1" size={24} />
                 </div>
-
                 <div className="flex flex-col gap-2">
-                  <Typography variant={isDesktop ? 'body2' : 'body3'} weight="medium">
-                    Expert Success Squad
+                  <Typography variant="body2" weight="medium">
+                    {t('cardsLeft.items.2.title')}
                   </Typography>
                   <Typography variant="body3" className="text-description">
-                    Your dedicated crew of specialists, cheering you on and clearing every hurdle.
+                    {t('cardsLeft.items.2.description')}
                   </Typography>
                 </div>
               </div>
             </div>
+
             <div ref={ref1} className="flex justify-center items-center">
               <Lottie
                 lottieRef={lottieRef1}
@@ -125,55 +129,57 @@ export const JourneySection = () => {
             </div>
           </div>
         </div>
-        <div className="bg-secondary-300 rounded-2xl max-w-[565px]  p-8 max-md:max-w-full">
+
+        {/* Right card */}
+        <div className="bg-secondary-300 rounded-2xl max-w-[565px] p-8 max-md:max-w-full">
           <div className="flex flex-col gap-6">
             <Typography variant={isDesktop ? 'h5' : 'h6'} weight="medium">
-              Customized Training Paths
+              {t('cardsRight.title')}
             </Typography>
             <div className="flex flex-col gap-4">
               <div className="flex gap-4">
                 <div className="flex w-10 h-10 items-center justify-center bg-light-green rounded-lg border-primary-green/20 border shrink-0">
                   <ListIcon size={24} color="#02B5AC" />
                 </div>
-
                 <div className="flex flex-col gap-2">
                   <Typography variant={isDesktop ? 'body2' : 'body3'} weight="medium">
-                    Personalized Blueprints
+                    {t('cardsRight.items.0.title')}
                   </Typography>
                   <Typography variant="body3" className="text-description">
-                    Custom learning maps built for each role, level, and ambition.
+                    {t('cardsRight.items.0.description')}
                   </Typography>
                 </div>
               </div>
+
               <div className="flex gap-4">
                 <div className="flex w-10 h-10 items-center justify-center bg-light-green rounded-lg border-primary-green/20 border shrink-0">
                   <ChartNotificationIcon color="#02B5AC" size={24} />
                 </div>
-
                 <div className="flex flex-col gap-2">
-                  <Typography variant={isDesktop ? 'body2' : 'body3'} weight="medium">
-                    Real-Time Progress Check
+                  <Typography variant="body2" weight="medium">
+                    {t('cardsRight.items.1.title')}
                   </Typography>
                   <Typography variant="body3" className="text-description">
-                    Instant insights on milestones that matter, so you can steer your team to success.
+                    {t('cardsRight.items.1.description')}
                   </Typography>
                 </div>
               </div>
+
               <div className="flex gap-4">
                 <div className="flex w-10 h-10 items-center justify-center bg-light-green rounded-lg border-primary-green/20 border shrink-0">
                   <TeacherIcon color="#02B5AC" size={24} />
                 </div>
-
                 <div className="flex flex-col gap-2">
-                  <Typography variant={isDesktop ? 'body2' : 'body3'} weight="medium">
-                    Strategic Coaching
+                  <Typography variant="body2" weight="medium">
+                    {t('cardsRight.items.2.title')}
                   </Typography>
                   <Typography variant="body3" className="text-description">
-                    Your own training advisor, aligning every lesson with your big-picture goals.
+                    {t('cardsRight.items.2.description')}
                   </Typography>
                 </div>
               </div>
             </div>
+
             <div ref={ref2} className="flex justify-center items-center">
               <Lottie
                 lottieRef={lottieRef2}
@@ -186,8 +192,9 @@ export const JourneySection = () => {
           </div>
         </div>
       </div>
+
       <Button variant="academy" className="max-md:w-full">
-        Build Your Branded Academy Now
+        {t('cta')}
       </Button>
     </section>
   )
