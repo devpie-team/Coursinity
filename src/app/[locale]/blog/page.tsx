b022dab65f087d70761fbb5d9ca0d67da4e9df70
Lokiner890 <rostick@offerwell.com>
2025-09-09 16:43:57 +0300

    Fix

--- src/app/[locale]/blog/page.tsx
+++ src/app/[locale]/blog/page.tsx
@@ -4,14 +4,23 @@ import { BlogClient } from './_components/BlockClient'
 
 type Search = { page?: string }
 
-export default async function BlogPage(_props: { searchParams: Search; params: { locale: string } }) {
+export default async function BlogPage({
+  searchParams,
+  params
+}: {
+  searchParams: Promise<Search>
+  params: Promise<{ locale: string }>
+}) {
+  const { locale } = await params
+  const { page: searchPage } = await searchParams
+
   const cookieStore = await cookies()
   const pageCookie = cookieStore.get('blog_page')?.value
 
-  const page = Math.max(1, Number(pageCookie ?? 1))
+  const page = Math.max(1, Number(searchPage ?? pageCookie ?? 1))
 
-  const data = await getArticles({ page, pageSize: 9, locale: _props.params.locale })
-  const dataFeatured = await getArticles({ featured: true, page: 1, pageSize: 4, locale: _props.params.locale })
+  const data = await getArticles({ page, pageSize: 9, locale })
+  const dataFeatured = await getArticles({ featured: true, page: 1, pageSize: 4, locale })
 
   return <BlogClient articles={data} featuredArticles={dataFeatured} currentPage={page} />
 }
