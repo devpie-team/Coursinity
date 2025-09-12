8198735b87cbae9cdd370ae48147194213692517
Lokiner890 <rostick@offerwell.com>
2025-09-12 12:21:48 +0300

    Fixed link

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -134,8 +134,13 @@ export const Header = () => {
               }}
             />
             <div className="flex flex-col gap-4 items-center">
-              <div
-                className="gap-[10px] flex items-center cursor-pointer select-none "
+              <a className="py-[7px] px-2" href={`/${locale}/solutions`}>
+                <Typography weight="medium" variant="h6">
+                  {t('solutions')}
+                </Typography>
+              </a>
+              {/* <div
+                className="gap-[10px] flex items-center cursor-pointer select-none"
                 onClick={() => setOpened((v) => !v)}>
                 <Typography weight="medium" variant="h6">
                   {t('services')}
@@ -144,16 +149,12 @@ export const Header = () => {
                   <CaretDown />
                 </div>
               </div>
-              {opened && (
-                <div className="flex flex-col gap-[6px]">
-                  <a className="py-[7px] px-2" href={`/${locale}/solutions`}>
-                    <Typography>{t('solutions')}</Typography>
-                  </a>
-                </div>
-              )}
-              <Typography weight="medium" variant="h6">
-                {t('blog')}
-              </Typography>
+              {opened && <div className="flex flex-col gap-[6px]"></div>}
+              <a href="">
+                <Typography weight="medium" variant="h6">
+                  {t('blog')}
+                </Typography>
+              </a> */}
             </div>
           </div>
           <a href={`/${locale}/contact-form`}>
