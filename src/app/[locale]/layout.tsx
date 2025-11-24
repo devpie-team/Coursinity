696d1565fca4908da14bf95551f4450375dfdac9
MikhaylovskiyPavlo <mikhaylovskiy05@gmail.com>
2025-11-24 13:22:31 +0200

    added clarity tag

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -152,6 +152,15 @@ export default async function LocaleLayout({
             gtag('config', 'G-4KH6HKMYCG');
           `}
         </Script>
+        <Script id="microsoft-clarity" strategy="afterInteractive">
+          {`
+            (function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "uak9ojw8g7");
+          `}
+        </Script>
         <link rel="icon" type="image/png" href="/assets/favicon-96x96.png" sizes="96x96" />
         <link rel="icon" type="image/svg+xml" href="/assets/favicon.svg" />
         <link rel="shortcut icon" href="/assets/favicon.ico" />
