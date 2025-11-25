4a072412c6f450a3e83e1be1dc5685b6cac30c0f
VolodimirDeveloper <lokinerlokiner@gmail.com>
2025-11-25 14:59:16 +0100

    Fixed bug

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -161,7 +161,8 @@ export default async function LocaleLayout({
               script.src = 'https://w.app/widget-v1/6qaFoB.js';
               d.getElementsByTagName('head')[0].appendChild(script);
             })(document);
-            `}
+            `}{' '}
+        </Script>
         <Script id="microsoft-clarity" strategy="afterInteractive">
           {`
             (function(c,l,a,r,i,t,y){
